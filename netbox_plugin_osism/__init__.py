093860ae1711257f487dd0e775afc81ed6fac870
sbstnnmnn <138126422+sbstnnmnn@users.noreply.github.com>
2023-11-15 12:44:06 +0100

    Add Zuul job python-black and format Python files (#12)
    Signed-off-by: Sebastian Neumann <neumann@b1-systems.de>

--- netbox_plugin_osism/__init__.py
+++ netbox_plugin_osism/__init__.py
@@ -3,20 +3,20 @@ from .version import __version__
 
 
 class OSISMConfig(PluginConfig):
-    name = 'netbox_plugin_osism'
-    verbose_name = 'OSISM'
-    description = 'OSISM integration'
+    name = "netbox_plugin_osism"
+    verbose_name = "OSISM"
+    description = "OSISM integration"
     version = __version__
-    author = 'OSISM GmbH'
-    author_email = 'info@osism.tech'
-    base_url = 'osism'
+    author = "OSISM GmbH"
+    author_email = "info@osism.tech"
+    base_url = "osism"
     required_settings = []
-    min_version = '3.2.0'
-    max_version = '3.4.99'
+    min_version = "3.2.0"
+    max_version = "3.4.99"
     default_settings = {
-        'grafana': 'http://localhost:3000',
-        'netdata': 'http://localhost:19999'
+        "grafana": "http://localhost:3000",
+        "netdata": "http://localhost:19999",
     }
 
 
-config = OSISMConfig # noqa
+config = OSISMConfig  # noqa
